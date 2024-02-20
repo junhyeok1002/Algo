2f17b04c4da9544d281b9a0876e5dd6b47fc489b
Jun Hyeok Choi <113586985+junhyeok1002@users.noreply.github.com>
2024-02-20 15:43:33 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int N;
+	static final int div_num = 10007;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(bf.readLine());
+		
+		int[] dp = new int[1001];
+		dp[1] = 1;
+		dp[2] = 3;
+		
+		for(int i = 3; i < dp.length; i++) {
+			dp[i] = (dp[i-1]+ dp[i-2]*2) % div_num;
+			if(i == N) break;
+		}
+		
+		System.out.println(dp[N]);
+	}
+	
+}
