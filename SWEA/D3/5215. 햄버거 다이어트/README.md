373317e4d8b5a6b405301a09f155e2f5609eecda
Jun Hyeok Choi <113586985+junhyeok1002@users.noreply.github.com>
2024-02-27 13:49:51 +0900

    [D3] Title: 햄버거 다이어트, Time: 145 ms, Memory: 29,804 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/README.md
@@ -0,0 +1,15 @@
+# [D3] 햄버거 다이어트 - 5215 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWT-lPB6dHUDFAVT) 
+
+### 성능 요약
+
+메모리: 29,804 KB, 시간: 145 ms, 코드길이: 2,570 Bytes
+
+### 제출 일자
+
+2024-02-27 13:49
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+	// Knapsack DP
+	static int[][] dp;
+	static int N, L;
+	static List<Ingredient> refrigerator;
+	static int max;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int test_case = Integer.parseInt(bf.readLine());
+		for(int t = 0; t < test_case ; t++) {
+			String[] line = bf.readLine().split(" ");
+			N = Integer.parseInt(line[0]);
+			L = Integer.parseInt(line[1]);
+			
+			// 각 열은 재료의 종류, 각 행은 L칼로리까지 물건을 넣었을때 최대밸류 0행은 편의 상 비워둠
+			dp = new int[L+1][N+1];
+			
+			max = 0;
+			refrigerator = new ArrayList<>();
+			refrigerator.add(new Ingredient()); // 빈거한개 넣기
+			
+			for(int i = 1 ; i < N+1; i++) {
+				line = bf.readLine().split(" ");
+				int taste = Integer.parseInt(line[0]);
+				int kcal = Integer.parseInt(line[1]);
+				
+				refrigerator.add(new Ingredient(taste, kcal));
+			}
+			// 무게 낮은 순으로 정렬후
+			Collections.sort(refrigerator);
+			
+			// dp 테이블을 채우기
+			fillDP();
+			
+			sb.append("#"+(t+1)+" "+ max +"\n");
+		}
+		System.out.println(sb);
+	}
+	private static void fillDP() {
+	for(int j = 1; j < N+1; j++) { // 물건 j
+		Ingredient ingredient = refrigerator.get(j);
+		
+		for(int i =1; i < L+1; i++) { // 무게 i
+			if(ingredient.kcal <= i) {
+				
+				// 최대 칼로리리가 i인 햄버거에 최대 맛을 정하려면
+				// 현재물건이 들어갔을때의 맛과 그렇지 않았을 때의 맛 중 최대 값이 되어야 한다
+				dp[i][j] = Math.max(dp[i][j-1], // 직전의 것과
+				ingredient.taste + dp[i-ingredient.kcal][j-1]); // 해당물건이 들어갔을때 최대를 비교 
+			}
+			// 들어갈 수 있는 것보다 현재 칼로리가 더 놓아버리면 이전것 대체
+			else {
+				dp[i][j] = dp[i][j-1];
+			}
+			// 최댓값 갱신
+			max = Math.max(max, dp[i][j]);
+		}
+	}
+}
+}
+class Ingredient<T> implements Comparable<T>{
+	int taste;
+	int kcal;
+	
+	public Ingredient() {}
+	public Ingredient(int taste, int kcal) {
+		this.taste = taste;
+		this.kcal = kcal;
+	}
+	
+	@Override
+	public int compareTo(T o) {
+		Ingredient ingredient = (Ingredient) o;
+		if(this.kcal > ingredient.kcal) return 1;
+		if(this.kcal < ingredient.kcal) return -1;
+			
+		if(this.taste > ingredient.taste) return 1;
+		if(this.taste < ingredient.taste) return -1;
+		
+		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "taste : " + taste + ", kcal : " + kcal+"\n";
+	}
+}
